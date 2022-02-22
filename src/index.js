aef667c5d922a34318f6db6b9fde31219c3edf3a
AMS2302 <ams23post@gmail.com>
2022-02-23 00:51:48 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBrackets = [];
+  let bracketsPairs = {};
+  bracketsConfig.forEach(element => {
+    openBrackets.push(element[0]);
+    bracketsPairs[element[1]] = element[0];
+  });
+  function checkStr(str) {
+    let array = str.split('');
+    let stack = [];
+    for (let index = 0; index < array.length; index++) {
+      const item = array[index];
+      if (openBrackets.includes(item) && bracketsPairs[item] !== item) {
+        stack.push(item);
+      } else if (bracketsPairs[item] === item && !stack.includes(item)) {
+        stack.push(item);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+        let top = stack[stack.length - 1];
+        if (bracketsPairs[item] === top) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+    return stack.length === 0;
+  }
+  return checkStr(str);
 }
